a5c179d45c4295253a66dcbb87b097b01b718ef3
alias <1214046231@qq.com>
2019-08-05 23:31:14 +0800

    update: add indexes component

---
+++ components/indexes/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class Indexes extends React.Component {
+    public render():JSX.Element {
+        return (
+            <div>
+                {/* is indexes component */}
+            </div>
+        )
+    }
+}
+
+export default Indexes;
